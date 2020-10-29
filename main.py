db9c04d27fef5fc53ea517a5e94fd59c3e170a02
Ashish Subedi <ashishsubedi10@gmail.com>
2020-10-30 00:58:12 +0545

    First prototype complete

--- main.py
+++ main.py
@@ -22,23 +22,42 @@ pygame.display.set_caption("Minesweeper")
 
 run = True
 firstClick = True
+gameOver = False
 
-mines = 20
-flags = 20
-
+mines = 30
+# flags = 20
 game_matrix = [[0 for i in range(cols)] for j in range(rows) ]
-print(game_matrix)
+
+
+def clear():
+    screen.fill(black)
+    for y in range(0,height,cell_h):
+        for x in range(0,width,cell_w):
+            
+            pygame.draw.rect(screen,white,(x,y,cell_w-1,cell_h-1))
+    pygame.display.update()
+
+def reset():
+    global firstClick,gameOver
+    clear()
+    firstClick = True
+    gameOver= False
+
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: run=False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_r:
+                reset()
         #Handle mouse event
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not gameOver:
             #Get position
             pos = pygame.mouse.get_pos()
             #Check the cell
             x,y = pos[0]//cell_w,pos[1]//cell_h
             #Initialize game
             if firstClick:
+                clear()
                 firstClick = False
                 
                 #Plant mines and start calculation
@@ -63,28 +82,36 @@ while run:
                 print("FINAL: ",game_matrix)           
 
             else:
-                if(game_matrix[x][y]>0): continue
-                elif game_matrix[x][y] == -1:
+                if game_matrix[x][y] == -1:
                     #Game over Mine BOOM
-                    pass 
+
+                    color = red
+                    pygame.draw.rect(screen,color,(x*cell_w,y*cell_h,cell_w-1,cell_h-1))
+                    gameOver = True
                 else:
-                    # game_matrix
-                    pass
+                    # Show score on that cell
+                    
+                    score = game_matrix[x][y]
+                    dest_x = (x*cell_w)+(cell_w/3)
+                    dest_y = (y*cell_h)+(cell_h/3)
+                    color = blue
+                    
+            
+                    pygame.draw.rect(screen,color,(x*cell_w,y*cell_h,cell_w-1,cell_h-1))
+                    screen.blit(font.render(str(score),True,black),(dest_x,dest_y))
+                    pygame.display.update()
+
+                    
 
 
             #display label
-    screen.fill(black)
-    # if firstClick:
-    for y in range(0,height,cell_h):
-        for x in range(0,width,cell_w):
-            i,j = x//cell_w,y//cell_h
-    
-            color = blue
-            if(game_matrix[i][j] == 0): color = white
-            elif(game_matrix[i][j] == -1): color = red
+    if firstClick:
+        clear()        
+        screen.blit(font.render(f"PRESS ANY CELL TO START GAME. {mines} Mines",True,black),(width/8,height/4))
+    if gameOver:   
+         
+        screen.blit(font.render("FOOOOSHHH!! GAME OVER. Press R",True,red),(width/8,height/4))
         
-            pygame.draw.rect(screen,color,(x,y,cell_w-1,cell_h-1))
-            
     pygame.display.update()
 
 pygame.quit()
