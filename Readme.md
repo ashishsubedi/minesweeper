d9c2a0558d5d9db7e8a1f7a12402e795a38d3e07
Ashish Subedi <ashishsubedi10@gmail.com>
2020-10-30 22:10:17 +0545

    Prototype v2. Most probably final build for now

--- Readme.md
+++ Readme.md
@@ -1,2 +1,23 @@
 # Minesweeper Game
-Download and run <code> python main.py </code> to play.
+
+## Description
+
+This is my version of minesweeper made in one and half day (more or less). This is very simple and basic so don't even expect much from this. Made using PyGame which I just learned during this build. 
+
+## How to Play
+
+- Download and run <code> python main.py </code> to play.
+- By default:
+    * 30 mines
+    * 30 flags
+    * 10x10 grid
+- ## You can change these values at top of the <code> main.py </code> file.
+
+### Controls
+
+- Left click to show number. The first cell you click is always safe.
+- Right click to flag. You have exactly same number of flags as mines.
+- White, Unknown
+- Blue, safe square
+- Red, Mine
+- Green, Flagged
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+'''
+    Author: Ashish Subedi
+    File: main.py
+    Description: Basic minesweeper game made using PyGame. Can be considered 2 day build. 
+
+'''
+
+
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+
 import pygame,sys,random
 from pygame.locals import *
 pygame.init()
+
+# Change these for different gameplay
+MINES = 30
+rows,cols = 10,10
+
+#End Change
 
 white = (255,255,255)
 gray = (150,150,150)
@@ -12,7 +30,6 @@ blue = (0,0,255)
 
 win_size = width,height = 600,600
 
-rows,cols = 10,10
 cell_w,cell_h= (width//cols),(height//rows)
 
 font = pygame.font.SysFont('Arial', 25)
@@ -23,11 +40,15 @@ pygame.display.set_caption("Minesweeper")
 run = True
 firstClick = True
 gameOver = False
+winner = False
 
-mines = 30
-# flags = 20
+mines = MINES
+flags = MINES
+
 game_matrix = [[0 for i in range(cols)] for j in range(rows) ]
+flagged = set()
 
+remainingSafe = 0
 
 def clear():
     screen.fill(black)
@@ -38,10 +59,16 @@ def clear():
     pygame.display.update()
 
 def reset():
-    global firstClick,gameOver
+    global firstClick,gameOver,mines,flags,flagged,game_matrix,remainingSafe,winner
     clear()
     firstClick = True
     gameOver= False
+    flagged = set()
+    game_matrix = [[0 for i in range(cols)] for j in range(rows) ]
+    mines = MINES
+    flags = MINES
+    remainingSafe = 0
+    winner = False
 
 while run:
     for event in pygame.event.get():
@@ -59,6 +86,8 @@ while run:
             if firstClick:
                 clear()
                 firstClick = False
+                #No of valid squares
+                remainingSafe = (rows*cols)-mines
                 
                 #Plant mines and start calculation
                 while mines > 0:
@@ -67,7 +96,6 @@ while run:
                     if(game_matrix[x_mine][y_mine] != -1 and (x_mine != x and y_mine != y) ):
                         game_matrix[x_mine][y_mine] = -1
                         mines -= 1
-                print("MINES:",game_matrix)
                 for j in range(rows):
                     for i in range(cols):
                         if(game_matrix[i][j] == -1): continue
@@ -79,18 +107,29 @@ while run:
                                         mine_count+=1
                         
                         game_matrix[i][j] = mine_count
-                print("FINAL: ",game_matrix)           
 
             else:
                 if game_matrix[x][y] == -1:
                     #Game over Mine BOOM
 
                     color = red
-                    pygame.draw.rect(screen,color,(x*cell_w,y*cell_h,cell_w-1,cell_h-1))
                     gameOver = True
+                    for j in range(rows):
+                        for i in range(cols):
+                            if game_matrix[i][j]==-1:
+                                pygame.draw.rect(screen,color,(i*cell_w,j*cell_h,cell_w-1,cell_h-1))
+                                #if the mine is flagged, show F
+                                if((i,j) in flagged):
+                                    dest_x = (i*cell_w)+(cell_w/3)
+                                    dest_y = (j*cell_h)+(cell_h/3)
+                                    
+                                    screen.blit(font.render("F",True,black),(dest_x,dest_y))
+
+
                 else:
+                    if (x,y) in flagged: continue
                     # Show score on that cell
-                    
+                    remainingSafe -= 1
                     score = game_matrix[x][y]
                     dest_x = (x*cell_w)+(cell_w/3)
                     dest_y = (y*cell_h)+(cell_h/3)
@@ -99,19 +138,71 @@ while run:
             
                     pygame.draw.rect(screen,color,(x*cell_w,y*cell_h,cell_w-1,cell_h-1))
                     screen.blit(font.render(str(score),True,black),(dest_x,dest_y))
-                    pygame.display.update()
+                  
 
-                    
+                    if( remainingSafe == 0):
+                        #Winner
+                        color = red
+                        gameOver = True
+                        winner = True
+
+                        for j in range(rows):
+                            for i in range(cols):
+                                if game_matrix[i][j]==-1:
+                                    pygame.draw.rect(screen,color,(i*cell_w,j*cell_h,cell_w-1,cell_h-1))
+                                    #if the mine is flagged, show F
+                                    if((i,j) in flagged):
+                                        dest_x = (i*cell_w)+(cell_w/3)
+                                        dest_y = (j*cell_h)+(cell_h/3)
+                                        
+                                        screen.blit(font.render("F",True,black),(dest_x,dest_y))
 
 
             #display label
+
+        #for Flags
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3 and not gameOver:
+            #Get position
+            pos = pygame.mouse.get_pos()
+            #Check the cell
+            x,y = pos[0]//cell_w,pos[1]//cell_h
+            if firstClick: continue
+
+            #Mine Flag it
+            if(flags and (x,y) not in flagged):
+                color = green
+                dest_x = (x*cell_w)+(cell_w/3)
+                dest_y = (y*cell_h)+(cell_h/3)
+
+                flagged.add((x,y))
+
+                pygame.draw.rect(screen,color,(x*cell_w,y*cell_h,cell_w-1,cell_h-1))
+                screen.blit(font.render('F',True,black),(dest_x,dest_y))
+                pygame.display.update()
+                flags-=1
+            elif((x,y) in flagged):
+                color = white
+                dest_x = (x*cell_w)+(cell_w/3)
+                dest_y = (y*cell_h)+(cell_h/3)
+
+                flagged.remove((x,y))
+
+                pygame.draw.rect(screen,color,(x*cell_w,y*cell_h,cell_w-1,cell_h-1))
+               
+                pygame.display.update()
+                flags+=1
+
+                
+
     if firstClick:
         clear()        
-        screen.blit(font.render(f"PRESS ANY CELL TO START GAME. {mines} Mines",True,black),(width/8,height/4))
+        screen.blit(font.render(f"CLICK START GAME. {mines} Mines.{flags} Flags",True,black),(0,height/4))
     if gameOver:   
-         
-        screen.blit(font.render("FOOOOSHHH!! GAME OVER. Press R",True,red),(width/8,height/4))
-        
+        if not winner:
+            screen.blit(font.render("FOOOOSHHH!! GAME OVER. Press R",True,black),(width/10,height/4))
+        else:
+            screen.blit(font.render("YOOHOO! WINNER. DONE. OVER. Press R for surprise",True,black),(width/10,height/4))
+
     pygame.display.update()
 
 pygame.quit()
